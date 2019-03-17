39f43c853da9c34af18c19071fb02151532ccb00
Andrew Ashton <aashtonnz@gmail.com>
2019-03-17 17:04:02 +1300

    3-34

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -7,10 +8,13 @@ class App extends Component {
       <div className="App">
 		<h1>Hi, I'm a React app</h1>
 		<p>This is really working!</p>
+		<Person />
+		<Person />
+		<Person />
       </div>
 	);
 	// Above JSX is compliled to:
-	// return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
+	// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 

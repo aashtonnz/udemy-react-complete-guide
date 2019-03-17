1be9a173683beb63e35cd08ac1528a6ad1068455
Andrew Ashton <aashtonnz@gmail.com>
2019-03-17 15:18:03 +1300

    3-30

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ class App extends Component {
     return (
       <div className="App">
 		<h1>Hi, I'm a React app</h1>
+		<p>This is really working!</p>
       </div>
-    );
+	);
+	// Above JSX is compliled to:
+	// return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 

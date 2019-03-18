6c05962ecf3a78b24feda3e8f61213a09b2b50ea
Andrew Ashton <aashtonnz@gmail.com>
2019-03-18 15:54:00 +1300

    3-45

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
   return (
-    <div onClick={props.click}>
+    <div className="Person" onClick={props.click}>
       <p>I am {props.name} and I am {props.age}!</p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} />
+      <input type="text" defaultValue={props.name} onChange={props.changed} />
     </div>
   );
 };

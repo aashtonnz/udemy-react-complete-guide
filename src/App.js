0dc5bf553f073b75f4ac0d708ee6ed239b645a2c
Andrew Ashton <aashtonnz@gmail.com>
2019-03-18 16:31:53 +1300

    4-51

--- src/App.js
+++ src/App.js
@@ -49,6 +49,26 @@ class App extends Component {
 			cursor: 'pointer',
 		};
 
+		let persons = null;
+		if (this.state.showPersons) {
+			persons = (
+				<div>
+					<Person
+						name={this.state.persons[0].name}
+						age={this.state.persons[0].age} />
+					<Person
+						name={this.state.persons[1].name}
+						age={this.state.persons[1].age}
+						click={this.switchNameHandler.bind(this, 'Greg!!!')}
+						changed={this.nameChangedHandler}>
+						My Hobby: Running</Person>
+					<Person
+						name={this.state.persons[2].name}
+						age={this.state.persons[2].age} />
+				</div>
+			);
+		}
+
 		return (
 			<div className="App">
 				<h1>Hi, I'm a React app</h1>
@@ -60,23 +80,7 @@ class App extends Component {
 					onClick={this.togglePersonsHandler}>
 					Toggle Persons
 				</button>
-				{
-					this.state.showPersons ?
-						<div>
-							<Person
-								name={this.state.persons[0].name}
-								age={this.state.persons[0].age} />
-							<Person
-								name={this.state.persons[1].name}
-								age={this.state.persons[1].age}
-								click={this.switchNameHandler.bind(this, 'Greg!!!')}
-								changed={this.nameChangedHandler}>
-								My Hobby: Running</Person>
-							<Person
-								name={this.state.persons[2].name}
-								age={this.state.persons[2].age} />
-						</div> : null
-				}
+				{persons}
 			</div>
 			// Above JSX is compliled to:
 			// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));

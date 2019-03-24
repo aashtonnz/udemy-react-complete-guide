13587a6e38aa3d16af7d876f668c1fed78ec368f
Andrew Ashton <aashtonnz@gmail.com>
2019-03-24 15:23:55 +1300

    7-89

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css'
 
 const cockpit = (props) => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    // Http request...
+    setTimeout(() => alert('Data loaded from cloud!'), 1000);
+  }, [])
+
+  // useEffect
+
   const assignedClasses = [];
   let btnClass = '';
 

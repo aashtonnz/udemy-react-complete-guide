72dbfd32183e7b49a9553e55e788ff8a35938e40
Andrew Ashton <aashtonnz@gmail.com>
2019-03-24 14:58:24 +1300

    7-86

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Person from './Person/Person'
 
-const persons = (props) => {
-  console.log('[Persons.js] rendering...')
-  return props.persons.map((person, index) => {
-    return <Person
-      name={person.name}
-      age={person.age}
-      click={() => props.clicked(index)}
-      key={person.id}
-      changed={(event) => props.changed(event, person.id)} />;
-  });
+class Persons extends Component {
+  // Doesn't make sense to use as state is not initialised:
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
+
+	// Legacy:
+  // componentWillReceiveProps(props) {
+  //   console.log('[Persons.js] componentWillRecieveProps');
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return { message: 'Shapshot!' };
+  }
+
+	// Legacy:
+  // componentWillUpdate(props) {
+  //   console.log('[Persons.js] componentWillUpdate');
+  // }
+
+  componentDidUpdate(prevProps, prevState, shapshot) {
+    console.log('[Persons.js] componentDidUpdate');
+    console.log(shapshot);
+  }
+
+  render() {
+    console.log('[Persons.js] rendering...');
+    return this.props.persons.map((person, index) => {
+      return <Person
+        name={person.name}
+        age={person.age}
+        click={() => this.props.clicked(index)}
+        key={person.id}
+        changed={(event) => this.props.changed(event, person.id)} />;
+    });
+  }
 }
 
-export default persons;
+export default Persons;

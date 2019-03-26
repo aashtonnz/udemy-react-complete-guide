3e9f6e0f58d3e49b2f53c27b5b12b066b21f7366
Andrew Ashton <andrew.ashton@vizexplorer.com>
2019-03-27 09:56:29 +1300

    7-89

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
     return (
-      <div className={classes.Person}>
+      // <div className={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age}!</p>
         <p>{this.props.children}</p>
         <input
           type="text"
           value={this.props.name}
           onChange={this.props.changed} />
-      </div>
+      </Aux>
+      // </div>
     );
   }
 };
